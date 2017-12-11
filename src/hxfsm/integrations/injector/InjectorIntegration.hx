fa2831f291dd6fa1d02c70e6f84483a825db0992
Leo Bergman <leo@pandacreative.se>
2017-12-11 19:06:01 +0100

    Use IInjectorState in InjectorIntegration.

--- src/hxfsm/integrations/injector/InjectorIntegration.hx
+++ src/hxfsm/integrations/injector/InjectorIntegration.hx
@@ -1,6 +1,5 @@
 package hxfsm.integrations.injector;
 
-import hxfsm.integrations.callback.ICallbackState;
 import minject.Injector;
 
 class InjectorIntegration implements IFSMIntegration {
@@ -10,12 +9,12 @@ class InjectorIntegration implements IFSMIntegration {
         _injector = injector;
     }
 
-    public function transition(newState:StateDef, oldState:StateDef) {
-        var newStateInstance:ICallbackState = cast Type.createInstance(newState.stateClass, []);
+    public function transition(newState:StateDef, oldState:StateDef):Void {
+        var newStateInstance:IInjectorState = cast Type.createInstance(newState.stateClass, []);
         _injector.injectInto(newStateInstance);
         newState.instance = newStateInstance;
         if (oldState != null) {
-            var oldStateInstance:ICallbackState = cast oldState.instance;
+            var oldStateInstance:IInjectorState = cast oldState.instance;
             oldStateInstance.exit();
         }
         newStateInstance.enter();
