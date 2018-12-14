08e285e1546878d81ce09bbe4e9fe84526a8ef1d
Leo Bergman <leo@hacksawstudios.com>
2018-12-14 16:18:36 +0100

    feat: Add transition guard

--- src/hxfsm/FSMController.hx
+++ src/hxfsm/FSMController.hx
@@ -1,23 +1,36 @@
 package hxfsm;
+
+import haxe.Timer;
+
 /**
-/* Controller to inject into application to perform transitions and query currentStateName
-/* Injecting FSM directly to would give access to add method, but adding states should be done during initial
-/* configuration. So provide access to FSMController instead to restrict usage to only the functionality needed after
-/* startup.
-*/
+	/* Controller to inject into application to perform transitions and query currentStateName
+	/* Injecting FSM directly to would give access to add method, but adding states should be done during initial
+	/* configuration. So provide access to FSMController instead to restrict usage to only the functionality needed after
+	/* startup.
+ */
 class FSMController {
-    public var currentStateName(get, null):String;
-    var _fsm:FSM;
+	public var currentStateName(get, null):String;
 
-    public function new(fsm:FSM) {
-        _fsm = fsm;
-    }
+	var _fsm:FSM;
 
-    public function goto(state:StateClass):Bool {
-        return _fsm.goto(state);
-    }
+	public function new(fsm:FSM) {
+		_fsm = fsm;
+	}
 
-    function get_currentStateName():String {
-        return _fsm._currentStateName;
-    }
+	public function goto(state:StateClass, ?guard:() -> Bool, wait = true):Bool {
+		if (guard != null) {
+			var allowed = guard();
+			if (!allowed && !wait) {
+				return false;
+			} else if (!allowed) {
+				Timer.delay(() -> goto(state, guard), 10);
+				return true;
+			}
+		}
+		return _fsm.goto(state);
+	}
+
+	function get_currentStateName():String {
+		return _fsm._currentStateName;
+	}
 }
